339639a6d3660225eba25242f750e0e732a958bd
Varulli <120419874+Varulli@users.noreply.github.com>
2024-06-13 17:50:27 -0400

    Set up rate limiter

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
+const compression = require("compression");
+const RateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+
 // Database connection setup
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
@@ -22,6 +26,12 @@ var usersRouter = require("./routes/users");
 
 var app = express();
 
+// Rate limiter setup
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 40, // limit each IP to 40 requests per windowMs
+});
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
@@ -31,6 +41,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(compression());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);

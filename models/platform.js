e4c68336d363b332f004be9a20714b305b8ee4c1
Varulli <120419874+Varulli@users.noreply.github.com>
2024-06-16 18:40:20 -0400

    Create platform model

---
+++ models/platform.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PlatformSchema = new Schema({
+  name: { type: String, required: true, max: 100, unique: true },
+});
+
+PlatformSchema.virtual("url").get(function () {
+  return "/inventory/platform/" + this.name.toLowerCase().replace(/ /g, "_");
+});
+
+module.exports = mongoose.model("Platform", PlatformSchema);

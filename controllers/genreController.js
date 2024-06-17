75edee8b533289d318512247f783536dd1a731aa
Varulli <120419874+Varulli@users.noreply.github.com>
2024-06-17 16:53:56 -0400

    Add items to sidebar and create template for create genre page

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -33,9 +33,9 @@ exports.genre_detail = asyncHandler(async (req, res, next) => {
 });
 
 // Display Genre create form on GET.
-exports.genre_create_get = function (req, res) {
-  res.send("NOT IMPLEMENTED: Genre create GET");
-};
+exports.genre_create_get = asyncHandler(async (req, res, next) => {
+  res.render("genre_form", { title: "Create Genre" });
+});
 
 // Handle Genre create on POST.
 exports.genre_create_post = function (req, res) {
@@ -44,6 +44,12 @@ exports.genre_create_post = function (req, res) {
 
 // Display Genre delete form on GET.
 exports.genre_delete_get = function (req, res) {
+  if (!isValidObjectId(req.params.id)) {
+    const err = new Error("Invalid Genre ID");
+    err.status = 404;
+    return next(err);
+  }
+
   res.send("NOT IMPLEMENTED: Genre delete GET");
 };
 
@@ -54,6 +60,12 @@ exports.genre_delete_post = function (req, res) {
 
 // Display Genre update form on GET.
 exports.genre_update_get = function (req, res) {
+  if (!isValidObjectId(req.params.id)) {
+    const err = new Error("Invalid Genre ID");
+    err.status = 404;
+    return next(err);
+  }
+
   res.send("NOT IMPLEMENTED: Genre update GET");
 };
 

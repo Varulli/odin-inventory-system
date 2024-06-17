5e0c7975416c0333d802c10ae84ae6769a451629
Varulli <120419874+Varulli@users.noreply.github.com>
2024-06-16 20:46:24 -0400

    Write functions for displaying list of genres and genre details

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -2,15 +2,21 @@ const debug = require("debug")("odin-inventory-system:genreController");
 
 const Genre = require("../models/genre");
 
+const asyncHandler = require("express-async-handler");
+
 // Display list of all Genres.
-exports.genre_list = function (req, res) {
-  res.send("NOT IMPLEMENTED: Genre list");
-};
+exports.genre_list = asyncHandler(async (req, res, next) => {
+  const genres = await Genre.find().sort({ name: 1 }).exec();
+  res.render("genre_list", { title: "List of Genres", genre_list: genres });
+});
 
 // Display detail page for a specific Genre.
-exports.genre_detail = function (req, res) {
-  res.send("NOT IMPLEMENTED: Genre detail: " + req.params.id);
-};
+exports.genre_detail = asyncHandler(async (req, res, next) => {
+  const [genre, genre_games] = await Promise.all([
+    Genre.findOne({ name: req.params.name }).exec(),
+    Genre.find({ name: req.params.name }).populate("games").exec(),
+  ]);
+});
 
 // Display Genre create form on GET.
 exports.genre_create_get = function (req, res) {
